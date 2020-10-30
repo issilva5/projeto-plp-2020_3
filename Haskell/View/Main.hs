ac6231921841b8c9737c6771f3ae374932da8459
Ítallo de Sousa Silva <itallo.silva@ccc.ufcg.edu.br>
2020-10-30 13:55:16 -0300

    Update Main.hs

--- Haskell/View/Main.hs
+++ Haskell/View/Main.hs
@@ -39,6 +39,31 @@ login dados = do
 
     proxMenu dados
 
+
+cadastra :: BD.BD -> IO()
+cadastra dados = do
+    putStrLn titleCadastro
+    putStrLn "(P)aciente"
+    putStrLn "(U)BS"
+    op <- prompt "Opção > "
+    
+    putStrLn ""
+
+    if toUpper (head op) == 'P' then do
+        dadosP <- lePaciente
+        senha <- prompt "Senha > "
+        inicial dados {BD.pacientes = (BD.pacientes dados) ++ [PC.criaPaciente (BD.nextID dados) dadosP], BD.logins = (BD.logins dados) ++ [(BD.nextID dados, senha, 0)], BD.idAtual = drop 1 (BD.idAtual dados)}
+
+    else if toUpper (head op) == 'U' then do
+        dadosU <- leUBS
+        senha <- prompt "Senha > "
+        inicial dados {BD.ubs = (BD.ubs dados) ++ [UBSC.criaUBS (BD.nextID dados) dadosU], BD.logins = (BD.logins dados) ++ [(BD.nextID dados, senha, 1)], BD.idAtual = drop 1 (BD.idAtual dados)}
+
+    else do
+        clear
+        inicial dados
+
+
 menuMedico :: Int -> BD.BD -> IO()
 menuMedico idMed dados = do
     putStrLn medicoMenu
@@ -106,29 +131,6 @@ menuMedico idMed dados = do
     else do
         clear
         menuMedico idMed dados
-
-cadastra :: BD.BD -> IO()
-cadastra dados = do
-    putStrLn titleCadastro
-    putStrLn "(P)aciente"
-    putStrLn "(U)BS"
-    op <- prompt "Opção > "
-    
-    putStrLn ""
-
-    if toUpper (head op) == 'P' then do
-        dadosP <- lePaciente
-        senha <- prompt "Senha > "
-        inicial dados {BD.pacientes = (BD.pacientes dados) ++ [PC.criaPaciente (BD.nextID dados) dadosP], BD.logins = (BD.logins dados) ++ [(BD.nextID dados, senha, 0)], BD.idAtual = drop 1 (BD.idAtual dados)}
-
-    else if toUpper (head op) == 'U' then do
-        dadosU <- leUBS
-        senha <- prompt "Senha > "
-        inicial dados {BD.ubs = (BD.ubs dados) ++ [UBSC.criaUBS (BD.nextID dados) dadosU], BD.logins = (BD.logins dados) ++ [(BD.nextID dados, senha, 1)], BD.idAtual = drop 1 (BD.idAtual dados)}
-
-    else do
-        clear
-        inicial dados
 
 menuPaciente :: BD.BD -> IO()
 menuPaciente dados = do 
