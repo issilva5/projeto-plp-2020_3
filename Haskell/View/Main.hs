bd33f81fe4d51c07f15eb8d2b84e1562b6e4c6c2
Ítallo de Sousa Silva <itallo.silva@ccc.ufcg.edu.br>
2020-10-31 00:35:30 -0300

    Update Main.hs

--- Haskell/View/Main.hs
+++ Haskell/View/Main.hs
@@ -46,6 +46,34 @@ login dados = do
     else do
         inicial dados
 
+cadastra :: BD.BD -> IO()
+cadastra dados = do
+    putStrLn titleCadastro
+    putStrLn "(P)aciente"
+    putStrLn "(U)BS"
+    op <- prompt "Opção > "
+    
+    putStrLn ""
+    if toUpper (head op) == 'P' then do
+        dadosP <- lePaciente
+        senha <- prompt "Senha > "
+        inicial dados {BD.pacientes =
+            (BD.pacientes dados) ++ [PC.criaPaciente (BD.nextID dados) dadosP], BD.logins =
+                (BD.logins dados) ++ [(BD.nextID dados, senha, 0)], BD.idAtual =
+                    drop 1 (BD.idAtual dados)}
+
+    else if toUpper (head op) == 'U' then do
+        dadosU <- leUBS
+        senha <- prompt "Senha > "
+        inicial dados {BD.ubs =
+            (BD.ubs dados) ++ [UBSC.criaUBS (BD.nextID dados) dadosU], BD.logins =
+                (BD.logins dados) ++ [(BD.nextID dados, senha, 1)], BD.idAtual =
+                    drop 1 (BD.idAtual dados)}
+
+    else do
+        clear
+        inicial dados
+
 menuPaciente :: Int -> BD.BD -> IO()
 menuPaciente idPac dados = do 
     putStrLn "Paciente"
@@ -212,31 +240,3 @@ menuMedico idMed dados = do
     else do
         clear
         menuMedico idMed dados
-
-cadastra :: BD.BD -> IO()
-cadastra dados = do
-    putStrLn titleCadastro
-    putStrLn "(P)aciente"
-    putStrLn "(U)BS"
-    op <- prompt "Opção > "
-    
-    putStrLn ""
-    if toUpper (head op) == 'P' then do
-        dadosP <- lePaciente
-        senha <- prompt "Senha > "
-        inicial dados {BD.pacientes =
-            (BD.pacientes dados) ++ [PC.criaPaciente (BD.nextID dados) dadosP], BD.logins =
-                (BD.logins dados) ++ [(BD.nextID dados, senha, 0)], BD.idAtual =
-                    drop 1 (BD.idAtual dados)}
-
-    else if toUpper (head op) == 'U' then do
-        dadosU <- leUBS
-        senha <- prompt "Senha > "
-        inicial dados {BD.ubs =
-            (BD.ubs dados) ++ [UBSC.criaUBS (BD.nextID dados) dadosU], BD.logins =
-                (BD.logins dados) ++ [(BD.nextID dados, senha, 1)], BD.idAtual =
-                    drop 1 (BD.idAtual dados)}
-
-    else do
-        clear
-        inicial dados
